96144760ccf6b4636ed760f96b64907e9e46e1f9
Tvitelli07 <thiagovitelli07@gmail.com>
2024-06-18 01:44:10 -0300

    PreEntregajs

---
+++ prueba.js
@@ -0,0 +1,35 @@
+function hola () {
+    alert('Hola ' + nombre + ' ,Bienvenido a Neil');
+};
+
+
+
+
+
+
+
+
+
+
+// Bienvenida
+    let nombre = prompt('Ingrese su nombre');
+    
+    hola ();
+
+
+// nombre Que quieres comprar?
+
+// if lo voy a usar si quiere cancelar la compra, ALGORITMO CONDICIONAL
+
+let queQuieresComprar = prompt( nombre + ', Queres Comprar\n 1: Zapatillas \n 2:Ropa');
+
+switch (queQuieresComprar){
+    case 1: 
+    alert('El valor de Zapatilla para salir es de $30000')
+    case 2:
+    alert('El valor de Zapatilla para correr es de $25000')
+}
+
+// Saludo
+
+
---
+++ zapatillas.js
@@ -0,0 +1,70 @@
+function saludo (){
+    alert('¡Hola ' + nombre + ', Bienvenido a Neil!')
+}
+
+let nombre =prompt('Ingrese su Nombre')
+saludo ()
+
+
+
+
+let producto = '';
+let cantidad = 0;
+let precio = 0;
+let seguirComprando = false;
+let precioFinal = 0;
+let productosFinal = 0;
+
+
+do {
+
+  producto = prompt( nombre + ' ¿Queres comprar Zapatillas o Indumentaria?').toLowerCase();
+
+  switch (producto) {
+    case 'zapatillas':
+    let zapatillasModelo = prompt('¿Que modelo de zapatilla buscas?\n 1: Zapatos?\n 2: Ojotas?')
+        if (zapatillasModelo == 1){
+            alert('Elegiste Zapatos, valen $20000');
+            precio = 20000
+        } else if (zapatillasModelo == 2){
+            alert('Elegiste Ojotas, valen $15000');
+            precio = 15000
+        } else {
+            alert('¡No existe ese modelo!');
+        }
+        break
+
+    case 'indumentaria':
+    let indumentariaModelo = prompt('¿Que modelo de Indumentaria buscas?\n 1: Pantalon?\n 2: Campera?')
+      if (indumentariaModelo == 1){
+            alert('Elegiste Pantalon, vale $5000');
+            precio = 5000
+        } else if (indumentariaModelo == 2){
+            alert('Elegiste Campera, vale $10000');
+            precio = 10000
+        } else {
+            alert('¡No existe ese modelo!');
+        }
+        break
+     default:
+        alert('¡No existe el producto!');
+  }
+
+    precioFinal += precio
+    // productosFinal += producto
+  
+  seguirComprando = confirm('¿Quieres comprar algo mas?')
+
+}while (seguirComprando);
+  
+
+
+// console.log(producto);
+// console.log(precio);
+console.log(precioFinal);
+// console.log(productosFinal);
+
+
+
+// //   seguirComprando = confirm('Desea seguir comprando?');
+
